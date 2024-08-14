5aa1809d1944c0fc22befcbd1f0064d5ca53a413
gunesdevin <hhghjhghgjghjghh@gmail.com>
2024-08-14 20:40:09 +0300

    Update uygulama.py
    for real kelimesini ekledim

--- uygulama.py
+++ uygulama.py
@@ -1,6 +1,7 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "FR": "for real / cidden",
             }
             
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
